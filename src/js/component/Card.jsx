9b4a2f0bb376f6f6bd51099b6a3867093738209f
danilopgon <113547781+danilopgon@users.noreply.github.com>
2023-06-02 11:34:30 +0000

    Add suggestions in Issue #4

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -5,13 +5,15 @@ import PropTypes from 'prop-types';
 const Card = ({ image, title, description, buttonText }) => {
   return (
 
-    <div className="card" >
-      <img src={image} className="card-img-top" alt=" " />
-      <div className="card-body">
-        <h5 className="card-title text-center">{title}</h5>
-        <p className="card-text  text-center">{description}</p>
+    <div className="col">
+      <div className="card w-100 w-lg-25" >
+        <img src={image} className="card-img-top" alt=" " />
+        <div className="card-body ">
+          <h5 className="card-title text-center">{title}</h5>
+          <p className="card-text  text-center">{description}</p>
+        </div>
+        <div className="card-footer text-center"><a href="#" className="btn btn-primary">{buttonText}</a></div>
       </div>
-      <div className="card-footer text-center"><a href="#" className="btn btn-primary">{buttonText}</a></div>
     </div>
   )
 };
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar.jsx";
 import Jumbotron from "./Jumbotron.jsx";
 import Card from "./Card.jsx";
 import Footer from "./Footer.jsx";
 
-
-//include images into your bundle
-
-
 //create your first component
 const Home = () => {
+  const [cardContent, setCardContent] = useState([
+    {
+      title: "First card", image: "https://cdn.pixabay.com/photo/2016/02/18/18/37/puppy-1207816_640.jpg",
+      description: "This is a puppy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam.",
+      buttonText: "First button"
+    },
+    {
+      title: "Second card", image: "https://cdn.pixabay.com/photo/2016/03/27/21/16/pet-1284307_640.jpg",
+      description: "This is a puppy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam.",
+      buttonText: "Second button"
+    },
+    {
+      title: "Third card", image: "https://cdn.pixabay.com/photo/2019/03/27/15/24/animal-4085255_640.jpg",
+      description: "This is a puppy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam.",
+      buttonText: "Third button"
+    },
+    {
+      title: "Fourth card", image: "https://cdn.pixabay.com/photo/2019/05/28/05/47/puppy-4234435_640.jpg",
+      description: "This is a puppy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam.",
+      buttonText: "Fourth button"
+    },
+  ]);
+
+
   return (
     <div className="container-fluid m-0 p-0">
       <div className="row d-flex justify-content-center m-0">
@@ -19,19 +39,10 @@ const Home = () => {
         <div className="row d-flex justify-content-center">
           <div className="col-12 col-lg-10">
             <Jumbotron />
-            <div className="d-flex flex-column flex-lg-row gap-3">
-              <Card title={"First card"} image={"https://cdn.pixabay.com/photo/2016/02/18/18/37/puppy-1207816_640.jpg"} 
-                description={"This is a puppy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam."}
-                buttonText={"First button"} />
-              <Card title={"Second card"} image={"https://cdn.pixabay.com/photo/2016/03/27/21/16/pet-1284307_640.jpg"} 
-              description={"This is another puppy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam."} 
-              buttonText={"Second button"} />
-              <Card title={"Third card"} image={"https://cdn.pixabay.com/photo/2019/03/27/15/24/animal-4085255_640.jpg"} 
-              description={"Yet another puppy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam."} 
-              buttonText={"Third button"} />
-              <Card title={"Fourth card"} image={"https://cdn.pixabay.com/photo/2019/05/28/05/47/puppy-4234435_640.jpg"} 
-              description={"The last puppy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam."} 
-              buttonText={"Fourth button"} />
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+              {cardContent.map((card) => {
+                return <Card title={card.title} image={card.image} description={card.description} buttonText={card.buttonText} />
+              })}
             </div>
             <Footer />
           </div>
@@ -42,5 +53,3 @@ const Home = () => {
 };
 
 export default Home;
-
-
